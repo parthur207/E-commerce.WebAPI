ccc99bdced11f2522485c3158ba2c738cbd1cda9
Paulo Andrade <parthur207@gmail.com>
2025-04-20 00:44:06 -0300

    Criação das entidades: Product e Sale Criação  e estruturação dos modelos: Sale e Product

---
+++ E-commerce WEB API - Teste técnico Rota/Domain/Entities/SaleEntity.cs
@@ -0,0 +1,6 @@
+﻿namespace E_commerce_WEB_API___Teste_técnico_Rota.Domain.Entities
+{
+    public class SaleEntity : BaseEntity
+    {
+    }
+}
--- E-commerce WEB API - Teste técnico Rota/Domain/Models/SaleModel.cs
+++ E-commerce WEB API - Teste técnico Rota/Domain/Models/SaleModel.cs
@@ -2,5 +2,14 @@
 {
     public class SaleModel
     {
+        public int ProductId { get; set; }
+        public int UserId { get; set; }
+        public DateTime SaleDate { get; set; }
+        public decimal TotalValue { get; set; }
+        public string Status { get; set; } = string.Empty;
+
+        /*public string PaymentMethod { get; set; } = string.Empty;
+        public string DeliveryAddress { get; set; } = string.Empty;
+        public string DeliveryStatus { get; set; } = string.Empty;*/
     }
 }
