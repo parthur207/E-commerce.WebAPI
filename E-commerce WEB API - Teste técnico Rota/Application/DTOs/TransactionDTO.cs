7047f3660fbc1f9dc5b4a53dc59903417daf5ca3
Paulo Andrade <parthur207@gmail.com>
2025-04-21 14:22:12 -0300

    Modificações e ajustes nos atributos DTO para se aproximar do propósito central.

--- E-commerce WEB API - Teste técnico Rota/Application/DTOs/TransactionDTO.cs
+++ E-commerce WEB API - Teste técnico Rota/Application/DTOs/TransactionDTO.cs
@@ -5,10 +5,9 @@ namespace E_commerce_WEB_API___Teste_técnico_Rota.Application.DTOs
 {
     public class TransactionDTO
     {
-        public int ProductId { get; set; }
+        public List<(string, int)> ShoppingList { get; set; }//Nome do produto e a quantidade
         public int UserId { get; private set; }
         public DateTime TransactionDate { get; private set; }
-        public int Quantity { get; set; }
         public decimal TotalValue { get; private set; }
         public TransactionStatusEnum TransactionStatus { get; set; }
     }
--- E-commerce WEB API - Teste técnico Rota/Application/DTOs/UserDTO.cs
+++ E-commerce WEB API - Teste técnico Rota/Application/DTOs/UserDTO.cs
@@ -4,13 +4,15 @@ namespace E_commerce_WEB_API___Teste_técnico_Rota.Application.DTOs
 {
     public class UserDTO
     {
-        public UserDTO(string name, DateTime birthDate, string email, DateTime createdAt, bool isDeleted, string? phone, UserStatusEnum status)
+        public UserDTO(string name, DateTime birthDate, string email, DateTime createdAt, bool isDeleted, string? phone, string address, UserStatusEnum status)
         {
             Name = name;
             BirthDate = birthDate;
             Email = email;
             CreatedAt = createdAt;
             Phone = phone;
+            Address = address;
+            TransactionsList = new List<(string, int)>();
             Status = status;
         }
 
@@ -18,9 +20,8 @@ namespace E_commerce_WEB_API___Teste_técnico_Rota.Application.DTOs
         public DateTime BirthDate { get; private set; }
         public string Email { get; private set; }
         public DateTime CreatedAt { get; private set; }
-        public bool IsDeleted { get; private set; }
         public string? Phone { get; private set; }
-
+        public string Address { get; private set; } 
         public List<(string,int)> TransactionsList { get; set; }
         public UserStatusEnum Status { get; private set; }
     }
