9c731d997e37b418f68f238c3444b6629f597a57
Paulo Andrade <parthur207@gmail.com>
2025-04-21 18:03:55 -0300

    Criação dos tipos de usuarios da plataforma. 'Roles' em enum.

--- E-commerce WEB API - Teste técnico Rota/Application/Interfaces/IProductInterface.cs
+++ E-commerce WEB API - Teste técnico Rota/Application/Interfaces/IProductInterface.cs
@@ -1,6 +1,21 @@
-﻿namespace E_commerce_WEB_API___Teste_técnico_Rota.Application.Interfaces
+﻿using E_commerce_WEB_API___Teste_técnico_Rota.Domain.Entities;
+
+namespace E_commerce_WEB_API___Teste_técnico_Rota.Application.Interfaces
 {
     public interface IProductInterface
     {
+        
+        //Queries
+        Task<List<ProductEntity>> GetAllProducts();
+
+        Task<ProductEntity> GetProductByName(string ProductName);
+
+        Task<List<ProductEntity>> GetProductsByCategory(string category);
+
+        Task<List<ProductEntity>> GetProductsByPrice(decimal price);
+
+        //Commands
+
+        Task<  >
     }
 }
---
+++ E-commerce WEB API - Teste técnico Rota/Domain/Enuns/UserTypeEnum.cs
@@ -0,0 +1,9 @@
+﻿namespace E_commerce_WEB_API___Teste_técnico_Rota.Domain.Enuns
+{
+    public enum UserTypeEnum
+    {
+        //roles
+        Admin= 1,
+        User = 2,
+    }
+}
