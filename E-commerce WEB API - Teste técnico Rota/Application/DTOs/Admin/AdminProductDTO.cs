657bd924f7eb0e25f456a89d537bf2d19d7fa52e
Paulo Andrade <parthur207@gmail.com>
2025-04-21 14:34:29 -0300

    Ajustes no Admin DTO product.

--- E-commerce WEB API - Teste técnico Rota/Application/DTOs/Admin/AdminProductDTO.cs
+++ E-commerce WEB API - Teste técnico Rota/Application/DTOs/Admin/AdminProductDTO.cs
@@ -1,23 +1,31 @@
-﻿using E_commerce_WEB_API___Teste_técnico_Rota.Domain.Enuns;
+﻿using E_commerce_WEB_API___Teste_técnico_Rota.Domain.Entities;
+using E_commerce_WEB_API___Teste_técnico_Rota.Domain.Enuns;
 
 namespace E_commerce_WEB_API___Teste_técnico_Rota.Application.DTOs.Admin
 {
     public class AdminProductDTO
     {
-        public AdminProductDTO(string productName, string description, decimal price, int quantity, ProductCategoryEnum category)
+        public AdminProductDTO(string productName, string description, decimal price, int quantity, int sales, ProductCategoryEnum category, string imagemUrl, ProductStatusEnum productStatus)
         {
             ProductName = productName;
             Description = description;
             Price = price;
             Quantity = quantity;
+            Sales = sales;
             Category = category;
+            ImageUrl=imagemUrl;
+            ProductStatus = productStatus;
         }
 
-        public string ProductName { get; set; }
-        public string Description { get; set; }
-        public decimal Price { get; set; }
-        public int Quantity { get; set; }
-        public ProductCategoryEnum Category { get; set; }
+
+        public string ProductName { get; private set; }
+        public string Description { get; private set; }
+        public decimal Price { get; private set; }
+        public int Quantity { get; private set; }
+        public int Sales { get; private set; }
+        public ProductCategoryEnum Category { get; private set; }
+        public string? ImageUrl { get; private set; }
+        public ProductStatusEnum ProductStatus { get; private set; }
 
     }
 }
