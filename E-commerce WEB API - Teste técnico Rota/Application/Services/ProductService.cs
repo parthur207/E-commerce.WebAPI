dde6cd4917bd140a5e99dc0857c0fafe0c4d7cb8
Paulo Andrade <parthur207@gmail.com>
2025-04-22 16:15:30 -0300

    Instalação das bibliotecas para implementar o JWT. Pré-implementação dos serviços da interface 'product'.

--- E-commerce WEB API - Teste técnico Rota/Application/Services/ProductService.cs
+++ E-commerce WEB API - Teste técnico Rota/Application/Services/ProductService.cs
@@ -1,8 +1,28 @@
 ﻿using E_commerce_WEB_API___Teste_técnico_Rota.Application.Interfaces;
+using E_commerce_WEB_API___Teste_técnico_Rota.Domain.Entities;
 
 namespace E_commerce_WEB_API___Teste_técnico_Rota.Application.Services
 {
     public class ProductService : IProductInterface
     {
+        public Task<List<ProductEntity>> GetAllProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProductEntity> GetProductByName(string ProductName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ProductEntity>> GetProductsByCategory(string category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ProductEntity>> GetProductsByPrice(decimal price)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
